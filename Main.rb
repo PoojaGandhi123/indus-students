4a8831500a07cd87c43f5f3f699fdfdb1f9fb8db
Pooja Gandhi <pgandhi3@gmail.com>
2016-05-27 20:24:20 -0700

    initial commit with sinatra app

---
+++ Main.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require './Student'
+require 'sinatra/flash'
+
+enable :sessions
+
+get '/register' do
+  @title = "Register"
+  erb :register
+end
+
+get '/about' do
+  @title = "About Us"
+  erb :about
+end
+
+get '/contact' do
+  @title = "Contact Us"
+  erb :contact
+end
+
+get '/career' do
+  @title = "Careers"
+  erb :career
+end
+
+not_found do
+  erb :not_found
+end
